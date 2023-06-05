8bc176a89993e5c0404f62436bbb19adeafc7e0d
Celtuzed <uter222222@gmail.com>
2023-06-05 12:30:26 +0300

    asdfasdf

---
+++ WpfBudgedAccounting/Note.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBudgedAccounting
+{
+    internal class Note
+    {
+        private string Title;
+        private string Type;
+        private double Money;
+
+        public DateTime date { get; set; }
+
+        public string title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+
+        public double money
+        {
+            get { return Money; }
+            set { Money = value; }
+        }
+
+        public string type
+        {
+            get { return Type; }
+            set { Type = value; }
+        }
+    }
+}
